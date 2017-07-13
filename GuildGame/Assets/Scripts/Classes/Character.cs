d12c6f887cc56d0d2183726656192c97e22883f8
gooliluck <gooliluck@gmail.com>
2017-07-13 16:45:24 +0800

    戰鬥排版大概圖上傳

---
+++ GuildGame/Assets/Scripts/Classes/Character.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character
+{
+	public string name;
+	public int level;
+	public int nextLevel;
+	public int exp;
+	public int nextExp;
+
+
+	public Character()
+	{
+		name = "";
+		level = -1;
+		nextLevel = -1;
+		exp = -1;
+		nextExp = -1;
+
+	}
+
+	public Character(string _name, int _level, int _nextLevel, int _exp, int _nextExp)
+	{
+		name = _name;
+		level = _level;
+		nextLevel = _nextLevel;
+		exp = _exp;
+		nextExp = _nextExp;
+
+	}
+}
+
