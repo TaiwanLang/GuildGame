69ef5e4ad6f0254c7f88df6f7c38352ab52c61e9
gooliluck <gooliluck@gmail.com>
2017-08-19 08:13:00 +0800

    animation制作

---
+++ GuildGame/Assets/Scripts/Monster/MonsterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterController : MonoBehaviour {
+	// Use this for initialization
+	UISpriteAnimation monsteranimation;
+	int animationcount;
+	public int mostermaxani;
+	public GameObject obj;
+	void Start () {
+		animationcount = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		monsteranimation = this.GetComponent (UISpriteAnimation);
+		if (animationcount == mostermaxani) {
+			monsteranimation.namePrefix = "";
+		}
+	}
+
+}
